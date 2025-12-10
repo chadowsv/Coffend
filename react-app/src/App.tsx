b4a8bcf9757632c23a5a9859cafe7ef75d45f7c9
anthqny <pilamunga2005@gmail.com>
2025-12-09 19:43:46 -0500

    Mesas Sin eRRORES GIT

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,11 +4,8 @@ import Login from "./pages/login";
 import Menu from "./pages/menu";
 import Register from "./pages/register";
 import RegisterSuccess from "./pages/RegisterSuccess";
-<<<<<<< HEAD
 import Sucursales from "./pages/sucursales";
-=======
 import Mesas from "./pages/mesas";
->>>>>>> c3506b7 (Mesas)
 
 function App() {
   return (
@@ -17,11 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />          {/* Página principal */}
         <Route path="/login" element={<Login />} />    {/* Página de inicio de sesión */}
         <Route path="/menu" element={<Menu />} />    {/* Página de menu*/}
-<<<<<<< HEAD
         <Route path="/sucursales" element={<Sucursales />} /> {/* Página de sucursales */}
-=======
         <Route path="/mesas" element={<Mesas />} /> 
->>>>>>> c3506b7 (Mesas)
         <Route path="/register" element={<Register />} /> {/* Página de registro */}
         <Route path="/register/success" element={<RegisterSuccess />} /> {/* Página de registro exitoso */}
       </Routes>
